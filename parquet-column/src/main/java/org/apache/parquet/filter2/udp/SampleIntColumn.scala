a34dc9baee0a55d0ba461aa655e94e9c04cb5cef
Vladimir Vladimirov <vladimir.vladimirov@magnetic.com>
2015-10-21 18:48:20 -0400

    moved class to another package (java one)

--- parquet-column/src/main/java/org/apache/parquet/filter2/udp/SampleIntColumn.scala
+++ parquet-column/src/main/java/org/apache/parquet/filter2/udp/SampleIntColumn.scala
@@ -18,24 +18,24 @@
  */
 package org.apache.parquet.filter2.udp
 
+import java.io.Serializable
 import java.lang.{Integer => JInt}
+import java.util.Random
 
 import org.apache.parquet.filter2.predicate.{Statistics, UserDefinedPredicate}
 
-import scala.util.Random
-
 class SampleIntColumn(threshold: Double) extends UserDefinedPredicate[JInt] with Serializable {
-  lazy val random = { new Random() }
-  val myThreshold = threshold
-  override def keep(value: JInt): Boolean = {
-    random.nextFloat() < myThreshold
-  }
+   lazy val random = { new Random() }
+   val myThreshold = threshold
+   override def keep(value: JInt): Boolean = {
+     random.nextFloat() < myThreshold
+   }
 
-  override def canDrop(statistics: Statistics[JInt]): Boolean = false
+   override def canDrop(statistics: Statistics[JInt]): Boolean = false
 
-  override def inverseCanDrop(statistics: Statistics[JInt]): Boolean = false
+   override def inverseCanDrop(statistics: Statistics[JInt]): Boolean = false
 
-  override def toString: String = {
-    "%s(%f)".format(getClass.getName, myThreshold)
-  }
-}
+   override def toString: String = {
+     "%s(%f)".format(getClass.getName, myThreshold)
+   }
+ }
